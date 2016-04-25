6e1e7ae38932a112cf010df16d5f99dc258d9cbc
mijdavis2 <mdavis@sungevity.com>
2016-04-24 19:42:27 -0700

    :new: Allow for git init - add tag checkout utility

--- pygit.py
+++ pygit.py
@@ -3,11 +3,12 @@
    :platform: Unix, Windows
    :synopsis: Utility for using git naturally in python scripts.
 
-.. moduleauthors:: mijdavis2
+.. moduleauthors:: mijdavis2, pmaguire
 
 .. versionadd:: 1.0.0
 """
 import subprocess
+import os
 
 
 class InvalidRepoDirectory(Exception):
@@ -15,19 +16,24 @@ class InvalidRepoDirectory(Exception):
 
 
 class PyGit(object):
-    def __init__(self, repo_directory):
+    def __init__(self, repo_directory, new_repo=False):
         """
         :param repo_directory: "/path/to/repo"
         :type repo_directory: str
+        :param new_repo: Set to true if you want PyGit to run a git init.
         """
         self.repo_directory = repo_directory
-        git_check = subprocess.check_output(['git', 'rev-parse', '--git-dir'], cwd=self.repo_directory).split("\n")[0]
-        if git_check != '.git':
+        if new_repo:
+            self._git("init")
+        if not os.path.isdir(self.repo_directory) or \
+                not os.access(self.repo_directory, os.R_OK) or \
+                not os.access(self.repo_directory, os.W_OK) or \
+                not subprocess.check_output(['git', 'rev-parse', '--git-dir'], cwd=self.repo_directory).split("\n")[0]:
             raise InvalidRepoDirectory("Invalid git repo directory: '{}'.\n"
                                        "repo_directory must be a root repo directory "
-                                       "of git project.".format(self.repo_directory))
+                                       "of git project and you must have r/w permissions.".format(self.repo_directory))
 
-    def __call__(self, *args, **kargs):
+    def __call__(self, *args, **kwargs):
         return self._git(args[0])
 
     def _git(self, arg_string):
@@ -36,7 +42,7 @@ class PyGit(object):
 
         .. example::
 
-            git = PyGit("/path/to/repo").git
+            git = PyGit("/path/to/repo")
             git("status")
 
             [
@@ -59,5 +65,15 @@ class PyGit(object):
         :type arg_string: str
         :returns: The stdout of the requested git command split by '\n' in the form of a list
         """
-        arguments = ["git"] + arg_string.split(" ")
+        arguments = ["git"] + arg_string.split()
         return subprocess.check_output(arguments, cwd=self.repo_directory).split("\n")
+
+    def checkout_tag(self, tag_name):
+        self._git('fetch -p')
+        git_output = subprocess.check_output(["git", "ls-remote", "--tags"], cwd=self.repo_directory).split()
+        remote_tags = [x.replace('refs/tags/', '') for x in git_output if 'refs/tags/' in x]
+        if tag_name not in remote_tags:
+            raise Exception("No version tag exists in the current repo named: '{}'. ".format(tag_name))
+        print ("Confirmed that version tag: {} exists in the repo. "
+               "Checking out this version.".format(tag_name))
+        self._git("checkout " + tag_name)
