8a58f3af368010d03d2664579f23b1d425b0aa1d
mijdavis2 <mdavisinsc@gmail.com>
2016-05-01 17:13:11 -0700

    :up: Update release

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='pygit',
     packages=['pygit'],
-    version='0.1.0',
+    version='0.1.1',
     description='Easy to use git utility for python.',
     author='mijdavis2',
     author_email='mdavisinsc@gmail.com',
     url='https://github.com/mijdavis2/pygit',
-    download_url='https://github.com/mijdavis2/pygit/tarball/0.1.0',
+    download_url='https://github.com/mijdavis2/pygit/tarball/0.1.1',
     keywords=['git', 'source control', 'utility'],
     classifiers=[]
 )
